595d7e37a8046f5f7f267bcc535b655b88aa0ced
ivan <ippeng54@gmail.com>
2023-05-11 16:38:13 +0400

    script create tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+create table customers
+(
+	customer_id varchar(100) primary key,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+
+create table employees
+(
+	employee_id int primary key,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100),
+	title varchar(100),
+	birth_date varchar(100),
+	notes text
+);
+create table orders
+(
+	order_id int primary key,
+	customer_id varchar(100) REFERENCES customers(customer_id) NOT NULL
+	employee_id int REFERENCES customers(employees) NOT NULL,
+	order_date varchar(100),
+	ship_city varchar(100)
+);
